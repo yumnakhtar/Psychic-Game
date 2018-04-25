029b8e95b0fee5c3117efc3f2b0f7ed424350d2a
yumnaaa <yumnaakhtar@hotmail.com>
2018-04-24 17:10:58 -0700

    add pseudocode. random letter generate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,29 @@
-alert("it works!");
+//GUessing game. Computer chooses a random letter and user has to guess which letter it is. Gets certain number of tries. 
+
+//Additional stuff i'd like to add: a button to restart the game
+
+
+//Initialize all variables 
+var userInput;
+// var copmuterGuess
+var wins = 0;
+var losses = 0;
+var guessesLeft = 5;
+var allGuesses = [];
+    //guesses so far
+    //
+
+//code for computer to pick a random letter
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+while (allGuesses.length < 5) {
+  allGuesses += alphabet[Math.floor(Math.random() * alphabet.length)];
+} 
+console.log(allGuesses);
+//code to tell user to enter a guess
+
+//compare guess to compters randomly picked letter (use document.onkeyup = function())
+
+//if same you won! increment wins by 1. restart game without refreshing game
+
+//if guess is wrong, decrement guesses left. add the guess to the guesses so far. 
