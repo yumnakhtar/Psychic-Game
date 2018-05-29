efd7f430811fec6d447159be51fe94ea8ecee85e
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-05-29 13:23:30 -0700

    update README

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # Psychic-Game
 
-This is a game that has the computer generate a letter from the alphabet. Then it proceeds to ask the user to guess what letter the computer has generated. THe user has 5 tries. If the user does not get hte right letter, losses gets incremeneted. if the user correctly guesses the right letter, wins gets incremented by 1. The game continues as long as the user wants to continue playing. Game will restart by refreshing the page.
+In this game the computer randomly generates a letter from the alphabet. The user must guess what this letter is within 5 tries. If the user does not get the right letter they loose and losses gets incremeneted. On the other hand, if the user correctly guesses the right letter, wins gets incremented by 1. The game continues as long as the user wants to continue playing. 
 
-Built With
-Javascript
-HTML
-CSS
+## Getting Started
+* To visit the deployed site follow this link: [Psychic Game](https://yumnakhtar.github.io/Psychic-Game/)
 
-Author
-Yumna Akhtar
+* You can also run a git pull command on your terminal and run the application from your computer. This way you can make changes to the code.
 
 
-License
+## Built With
+* HTML
+* CSS
+* Javascript
+* JQuery
+
+## Author
+Yumna Akhtar - *Initial Work* - [Github](https://github.com/yumnakhtar)
+
+
+## License
 This project is licensed under the MIT License - see the LICENSE.md file for details
 
-Acknowledgments
-Berkeley Bootcamp
+## Acknowledgments
+A huge thank you to my intrsuctor and TA's for the help and support as well as W3Schools and Stack Overflow for helping me through each hurdle. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ window.onload = function() {
     //Initialize all variables 
     var userInput;
     var UpperGuess;
-    var allGuesses = [" "];
+    var allGuesses = [];
     var computerPick;
     var wins = 0;
     var losses = 0;
@@ -14,6 +14,9 @@ window.onload = function() {
 //computer picks a random letter
 function randomGenerator(){
     computerPick = alphabet[Math.floor(Math.random() * alphabet.length)]; 
+    $(".guessed").empty();
+    allGuesses.length = 0;
+    guessesLeft = 5;
     console.log("the random letter is: ", computerPick);
 }   
 
@@ -23,12 +26,12 @@ document.onkeydown = function(event) {
         if (event.keyCode >= 65 && event.keyCode <= 90) {
             if (checkDup(userInput) === false){
                 guessesLeft--;
-                (allGuesses).push(userInput);
                 $(".left").text(guessesLeft);
                 $(".guessed").append(userInput);
+                checkMatch(userInput);
             }
             else{
-                console.log("the check is true, its a dupe!")
+                console.log("its a dupe!")
             }
 
         }
@@ -51,7 +54,26 @@ function checkDup(input){
         return false; 
 }
 
-
+function checkMatch(currInput) {
+    if (currInput === computerPick){
+        wins++;
+        console.log("you win!");
+        $(".wins").text(wins);
+        randomGenerator();
+    }
+    else {
+        console.log("nahhh");
+        if (guessesLeft === 0){
+            console.log("you lose");
+            losses++;
+            $(".losses").text(losses);
+            randomGenerator();
+        }
+        else {
+            (allGuesses).push(currInput);
+        }
+    }
+}
 
 randomGenerator();
 }
